d04e60decf1a6b11aee0bb2fbd92df20e1814972
Meralis <lucy.klim.30@gmail.com>
2023-02-23 16:29:32 +0200

    add

---
+++ istock/src/main/java/org/klim/istock/IstockApplication.java
@@ -0,0 +1,13 @@
+package org.klim.istock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstockApplication.class, args);
+	}
+
+}
---
+++ istock/src/test/java/org/klim/istock/IstockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.klim.istock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

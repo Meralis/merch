d967b1f1c3bbfccaf13fc3b32d79887a09f072c6
Meralis <lucy.klim.30@gmail.com>
2023-03-02 13:23:27 +0200

    added Select categories

---
+++ istock/frontend/src/components/CategoriesSelect.js
@@ -0,0 +1,8 @@
+function CategoriesSelect({categories, setSelectedCategory}) {
+    return <select className={'form-control mt-3'} onChange={e => setSelectedCategory(e.currentTarget.value)}>
+        <option value=''>Всі</option>
+        {categories.map(category => <option key={category}>{category}</option>)}
+    </select>
+}
+
+export default CategoriesSelect;
--- istock/frontend/src/components/Products.js
+++ istock/frontend/src/components/Products.js
@@ -2,24 +2,32 @@ import {useEffect, useState} from "react";
 import {Col, Row} from "react-bootstrap";
 import Product from "./Product";
 import Cart from "./Cart";
+import CategoriesSelect from "./CategoriesSelect";
 
 function Products() {
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:8080/').then(data => data.json()).then(data => {
             setProducts(data.map(product => ({...product, addedToCart: false, count: 1})))
+            getAllCategories(data);
         })
+
     }, []);
 
-    // useEffect(()=> {
-    //     console.log(products);
-    // },[products])
+    function getAllCategories(products) {
+        const categories = products.reduce(
+            (acc, product) => (acc.includes(product.category) ? acc : [...acc, product.category]), [])
+        setCategories(categories);
+        console.log(categories);
+    }
 
     function addToCart(id) {
         const newProducts = products.map(product => ({
             ...product,
-            addedToCart: product.id === id ? true : product.addedToCart
+            addedToCart: id === product.id ? true : product.addedToCart
         }))
         setProducts(newProducts);
     }
@@ -35,12 +43,15 @@ function Products() {
     return <Row>
         <Col xs={12}>
             <Cart
-                products={products.filter(product => product === product.addedToCart)}
+                products={products.filter(product => product.addedToCart)}
                 removeFromCart={removeFromCart}/>
         </Col>
-        {products.map(product => <Product
-            key={product.id} product={product} addToCart={addToCart}
-            removeFromCart={removeFromCart}/>)}
+        <CategoriesSelect categories={categories} setSelectedCategory={setSelectedCategory}/>
+
+        {products.filter(product => product.category === selectedCategory || !selectedCategory)
+            .map(product => <Product key={product.id} product={product}
+                                     addToCart={addToCart} removeFromCart={removeFromCart}
+            />)}
     </Row>
 }
 

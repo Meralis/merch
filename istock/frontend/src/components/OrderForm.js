51c58091b43417917caf0479c148119f23dededa
Meralis <lucy.klim.30@gmail.com>
2023-03-11 18:03:54 +0200

    new OrderForm without sending

--- istock/frontend/src/components/OrderForm.js
+++ istock/frontend/src/components/OrderForm.js
@@ -1,81 +1,79 @@
 import {Button, Col, Form, Row} from "react-bootstrap";
 import React from "react";
 
-export default class OrderForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
-            address: ''
-        }
+export default function OrderForm() {
+    const [firstName, setFirstName] = React.useState('');
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const [lastName, setLastName] = React.useState('');
 
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value})
-    }
+    const [email, setEmail] = React.useState('');
 
-    handleSubmit(event) {
-        this.sendOrderData().then(data => data.json());
-        event.preventDefault();
-    }
+    const [phone, setPhone] = React.useState('');
 
-    async sendOrderData() {
-        const formData = new FormData;
-        formData.append("firstName", this.state.firstName);
-        formData.append("lastName", this.state.lastName);
-        formData.append("email", this.state.email);
-        formData.append("phone", this.state.phone);
-        formData.append("address", this.state.address);
-        const response = await fetch('http://localhost:8080/order', {
-            method: 'PUT',
-            body: formData
-        })
-        const data = await response.json();
-    }
+    const [address, setAddress] = React.useState('');
 
-    render() {
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <div className={'mt-4'}><h3>Ваші дані</h3></div>
-                <Row>
-                    <Form.Group as={Col} controlId="firstName">
-                        <Form.Label>Ім'я</Form.Label>
-                        <Form.Control type="text" value={this.state.value} name="firstName"
-                                      onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="lastName">
-                        <Form.Label>Прізвище</Form.Label>
-                        <Form.Control type="text" value={this.state.value} name="lastName"
-                                      onChange={this.handleChange}/>
-                    </Form.Group>
-                </Row>
-                <Row>
-                    <Form.Group as={Col} controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" value={this.state.value} name="email" onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="phone">
-                        <Form.Label>Телефон</Form.Label>
-                        <Form.Control type="tel" placeholder="+380" value={this.state.value} name="phone"
-                                      onChange={this.handleChange}/>
-                    </Form.Group>
-                </Row>
-                <div className={'mt-4'}><h3>Доставлення</h3></div>
-                <Form.Group controlId="address">
-                    <Form.Label>Адреса</Form.Label>
-                    <Form.Control value={this.state.value} name="address" onChange={this.handleChange}/>
+    const handleSubmit = React.useCallback((e) => {
+        console.log('имя', firstName);
+        e.preventDefault();
+    }, [firstName, lastName, email, phone, address])
+
+    const isSubmitActive = React.useMemo(() => {
+        return firstName !== '' && lastName !== '' && email !== '' && phone !== '' && address !== '';
+    }, [firstName, lastName, email, phone, address])
+
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <div className={'mt-4'}><h3>Ваші дані</h3></div>
+            <Row>
+                <Form.Group as={Col} controlId="firstName">
+                    <Form.Label>Ім'я</Form.Label>
+                    <Form.Control type="text" value={firstName} name="firstName"
+                                  onChange={(e) => setFirstName(e.target.value)}/>
                 </Form.Group>
-                <Button variant="primary" type="submit" className={'mt-4'}>
-                    Відправити
-                </Button>
-            </Form>
-        );
-    }
+                <Form.Group as={Col} controlId="lastName">
+                    <Form.Label>Прізвище</Form.Label>
+                    <Form.Control type="text" value={lastName} name="lastName"
+                                  onChange={(e) => setLastName(e.target.value)}/>
+                </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group as={Col} controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" value={email} name="email" onChange={(e) => setEmail(e.target.value)}/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="phone">
+                    <Form.Label>Телефон</Form.Label>
+                    <Form.Control type="tel" placeholder="+380" value={phone} name="phone"
+                                  onChange={(e) => setPhone(e.target.value)}/>
+                </Form.Group>
+            </Row>
+            <div className={'mt-4'}><h3>Доставлення</h3></div>
+            <Form.Group controlId="address">
+                <Form.Label>Адреса</Form.Label>
+                <Form.Control value={address} name="address" onChange={(e) => setAddress(e.target.value)}/>
+            </Form.Group>
+            <Button variant="primary" type="submit" className={'mt-4'} disabled={!isSubmitActive}>
+                Відправити
+            </Button>
+        </Form>
+    );
 }
+
+
+
+//     async sendOrderData() {
+//         const formData = new FormData;
+//         formData.append("firstName", this.state.firstName);
+
+
+//         const response = await fetch('http://localhost:8080/order', {
+//             method: 'PUT',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//
+//             })
+//         });
+//         const data = await response.json();
+
 
